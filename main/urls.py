d2e0adb2ab79d50284e89180d16d20d00436adae
Camila Adriana <camila.adriana.gomes@outlook.com>
2021-07-21 11:50:52 -0300

    :wrench: Atualizando funcionalides de Login

--- main/urls.py
+++ main/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="Logins/forgot.html"), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="Logins/ResetDone.html"), name="password_reset_done"),
     path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="Logins/reset.html"), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="Logins/PasswordResetComplete.html"), name="password_reset_complete"),
 
 
 ]
