136b451c7c9372956dbee31e6aca3f42a99b39bf
Camila Adriana <camila.adriana.gomes@outlook.com>
2021-07-30 16:16:07 -0300

    :bug: Admin completo

--- Usuarios/admin.py
+++ Usuarios/admin.py
@@ -7,7 +7,6 @@ class PessoaAdmin(admin.ModelAdmin):
     list_display = ('Nome', 'email', 'Cpf','estado','id') #Dentro do admin na class Pessoas, é esses atributos que será mostrado
     list_filter = ('is_active','is_superuser','is_staff')#Filtrar Pessoas por esses atributos
     date_hierarchy = 'DataCadastro'
-    # readonly_fields = ('DataCadastro','Nascimento','Cpf')#Altera aqueles campos, deixando somente para leitura
     search_fields = ['Nome']#Campo de pesquisa por nome
     view_on_site = False
 
